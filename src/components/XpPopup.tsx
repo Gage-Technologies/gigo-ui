36eb5f2b1c026c2f85cc03127e28da9edd7a43a2
Megan U <megan@gagepiracy.com>
2024-02-14 12:21:49 -0600

    added gigo pro popup after xp popup

--- src/components/XpPopup.tsx
+++ src/components/XpPopup.tsx
@@ -6,20 +6,30 @@ import {
     ThemeProvider,
     Box,
     Modal,
+    IconButton,
 } from "@mui/material";
 import { styled } from "@mui/material/styles";
-import {getAllTokens} from "../theme";
+import {getAllTokens, themeHelpers} from "../theme";
 import { keyframes } from "@emotion/react";
 import LinearProgress from "@mui/material/LinearProgress";
 import * as levelUp from "../img/levelUp.json"
 import * as XPBoost from "../img/doubleXP.json";
 import Lottie from "react-lottie";
 import {useEffect} from "react";
-import Button from "@mui/material/Button";
+import { Button } from "@mui/material"
 import {Fade} from "react-awesome-reveal"
 import LootPopup from "./LootPopup";
 import Joyride from "react-joyride";
 import {bottom} from "@popperjs/core";
+import { selectAuthState } from "../reducers/auth/auth";
+import { useAppSelector } from "../app/hooks";
+import premiumGorilla from "../img/pro-pop-up-icon-plain.svg"
+import proBackground from "../img/popu-up-backgraound-plain.svg"
+import fireworks from "../img/fireworks.svg"
+import croppedPremium from "../img/croppedPremium.png"
+import call from "../services/api-call";
+import config from "../config";
+import { Close } from "@material-ui/icons";
 
 interface IProps {
     oldXP: number;
@@ -36,10 +46,13 @@ interface IProps {
 
 const XpPopup = (props: IProps) => {
     let userPref = localStorage.getItem("theme");
-    const [mode, _] = React.useState<PaletteMode>(
-        userPref === "light" ? "light" : "dark"
-    );
-        const theme = React.useMemo(() => createTheme(getAllTokens(mode)), [mode]);
+    // const [mode, _] = React.useState<PaletteMode>(
+    //     userPref === "light" ? "light" : "dark"
+    // );
+    // const theme = React.useMemo(() => createTheme(getAllTokens(mode)), [mode]);
+
+    const [mode, setMode] = React.useState<PaletteMode>(userPref === 'light' ? 'light' : 'dark');
+    const theme = React.useMemo(() => createTheme(getAllTokens(mode)), [mode]);
 
     const [showConfetti, setShowConfetti] = React.useState(false);
 
@@ -54,6 +67,9 @@ const XpPopup = (props: IProps) => {
     const [showLoot, setShowLoot] = React.useState(false);
     const [reward, setReward] = React.useState(props.reward);
     const [renown, setRenown] = React.useState(props.renown + 1);
+    const [showPro, setShowPro] = React.useState(false);
+    const [proMonthlyLink, setProMonthlyLink] = React.useState("");
+    const [proYearlyLink, setProYearlyLink] = React.useState("");
 
     const [steps, setSteps] = React.useState([{
         content: <h2>Let's begin our journey!</h2>,
@@ -74,8 +90,14 @@ const XpPopup = (props: IProps) => {
     }
   `;
 
+  const authState = useAppSelector(selectAuthState);
+
 
     useEffect(() => {
+        let premium = authState.role.toString()
+        if (premium === "0") {
+            retrieveProUrls()
+        }
         if (props.levelUp) {
             setTimeout(function(){
                 setLootBox(true)
@@ -110,7 +132,10 @@ const XpPopup = (props: IProps) => {
     };
 
     const confirmButton = () => {
-        if (lootBox) {
+        let premium = authState.role.toString()
+        if (premium === "0") {
+            setShowPro(true)
+        } else if (lootBox) {
             setShowLoot(true);
         } else {
             setOpen(false);
@@ -122,6 +147,31 @@ const XpPopup = (props: IProps) => {
         }
     }
 
+    const retrieveProUrls = async (): Promise<{ monthly: string, yearly: string } | null> => {
+        let res = await call(
+            "/api/stripe/premiumMembershipSession",
+            "post",
+            null,
+            null,
+            null,
+            // @ts-ignore
+            {},
+            null,
+            config.rootPath
+        )
+
+        if (res !== undefined && res["return url"] !== undefined && res["return year"] !== undefined) {
+            setProMonthlyLink(res["return url"])
+            setProYearlyLink(res["return year"])
+            return {
+                "monthly": res["return url"],
+                "yearly": res["return year"],
+            }
+        }
+
+        return null
+    }
+
     const closePopupLoot = () => {
         setOpen(false)
         if (props.popupClose !== null){
@@ -129,6 +179,188 @@ const XpPopup = (props: IProps) => {
         }
         window.sessionStorage.setItem("loginXP", "undefined")
         window.sessionStorage.setItem("attemptXP", "undefined")
+    }
+
+    const renderXPPopup = () => {
+        if (showLoot) {
+            return (
+                <div>
+                    <LootPopup closePopup={closePopupLoot}
+                           //@ts-ignore
+                           reward={props.reward}/>
+                 </div>
+            )
+        } else if (showPro) {
+            // @ts-ignore
+            return (
+                <>
+                    <style>
+                        {`
+                            /* Your existing animation styles */
+                            .card {
+                                border-radius: 10px;
+                                padding: 20px;
+                                margin: 10px;
+                                box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+                                text-align: center;
+                                width: 200px;
+                            }
+                            .plan-header {
+                                font-size: 20px;
+                                margin-bottom: 10px;
+                            }
+                            .plan-price {
+                                font-size: 24px;
+                                margin-bottom: 10px;
+                            }
+                            .plan-button {
+                                border: none;
+                                padding: 15px 32px;
+                                text-align: center;
+                                text-decoration: none;
+                                display: inline-block;
+                                font-size: 16px;
+                                margin: 4px 2px;
+                                cursor: pointer;
+                            }
+                            .promo-image {
+                                width: 30%; /* Make the image full width of the container */
+                                margin-bottom: 20px; /* Add space below the image */
+                            }
+                            .fireworks {
+                                width: 30%
+                            }
+                            .title {
+                                font-size: 34px; /* Larger font size for the title */
+                                margin-bottom: 10px; /* Space below the title */
+                                font-weight: bold;
+                            }
+                            .subtitle {
+                                font-size: 18px; /* Smaller font size for the subtitle */
+                                margin-bottom: 20px; /* Space below the subtitle */
+                            }
+                        `}
+                    </style>
+                    <div style={{
+                        borderRadius: "10px",
+                        padding: "20px",
+                        textAlign: "center"
+                    }}>
+                        <IconButton
+                            edge="end"
+                            color="inherit"
+                            size="small"
+                            onClick={() => {
+                                console.log("loot is: ", showLoot)
+                                if (lootBox) {
+                                    console.log("here")
+                                    setShowLoot(true);
+                                setShowPro(false);
+                                } else {
+                                setOpen(false);
+                                if (props.popupClose !== null) {
+                                    props.popupClose();
+                                }
+                                window.sessionStorage.setItem("loginXP", "undefined");
+                                window.sessionStorage.setItem("attemptXP", "undefined");
+                                }
+                            }}
+                            sx={{
+                                position: "absolute",
+                                top: '20vh',
+                                right: '38vw'
+                            }}
+                            >
+                            <Close />
+                        </IconButton>
+                        <img src={premiumGorilla} className="promo-image"/>
+                        <div className="title">GIGO Pro</div>
+                        <div className="subtitle">Get access to more resources and become a better developer today</div>
+                        <div style={{
+                            display: "flex",
+                            justifyContent: "center"
+                        }}>
+                            <div className="card" style={{
+                                backgroundColor:
+                                //@ts-ignore
+                                "#070D0D"}}>
+                                <div className="plan-header">1 Month</div>
+                                <div className="plan-price">$15 / MO</div>
+                                <Button variant="contained" onClick={() => window.open(proMonthlyLink, "_blank")} style={{backgroundColor: theme.palette.secondary.dark}}>Select</Button>
+                            </div>
+                            <div className="card" style={{
+                                backgroundColor:
+                                //@ts-ignore
+                                "#070D0D"}}>
+                                <div className="plan-header">12 Months</div>
+                                <div className="plan-price">$11.25 / MO</div>
+                                <Button variant="contained" onClick={() => window.open(proYearlyLink, "_blank")} style={{backgroundColor: theme.palette.secondary.dark}}>Select</Button>
+                            </div>
+                        </div>
+                    </div>
+                </>
+            )
+        } else {
+            return (
+                <div>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "start",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <Fade cascade damping={1e-1} direction={"left"}>
+                            <h1>{"You Earned " + xpTitle + " XP"}</h1>
+                        </Fade>
+                    </div>
+                    <div style={{ height: "5vh" }} />
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "start",
+                            justifyContent: "center",
+                        }}
+                    >
+                        <h3>{"Renown " +
+                            renown}</h3>
+                    </div>
+                    <div
+                        style={{
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            width: "100%",
+                        }}
+                    >
+                        <h4>{"Lvl " + currentLevel}</h4>
+                        <div style={{ width: "20px" }} />
+                        <Box sx={{ width: "75%"}}>
+                            <StyledLinearProgress variant="determinate"/>
+                        </Box>
+                        <div style={{ width: "20px" }} />
+                        <h4>{"Lvl " + nextLevel}</h4>
+                    </div>
+                    {/* {showConfetti && (
+                        <div style={{position: "absolute"}}>
+                            <Lottie options={confettiOptions} isClickToPauseDisabled={true}
+                                    width={window.innerHeight / .8}
+                                    height={window.innerHeight / .8} style={{zIndex: 4}}/>
+                        </div>
+                    )} */}
+                    <div style={{height: "5vh"}}/>
+                    <div style={{display: "flex", alignItems: "end", justifyContent: "center"}}>
+                        <Button variant={"contained"}
+                                sx={{ width: "25%", height: "60px", backgroundColor: "#29C18C", color: "white", borderRadius: "25px", boxShadow: '0 5px 0 #235d30',
+                                    '&:active':{transform: 'translateY(5px)', boxShadow: 'none'}, '&:hover':{backgroundColor: "#29C18C", cursor: 'pointer', '&:before': {transform: 'translateX(300px) skewX(-15deg)', opacity: 0.6, transition: '.7s'}}}} disableRipple={true} onClick={() => confirmButton()}
+                                id={"button"} className={'button'} style={{zIndex: "600000"}}
+                        >
+                            Confirm
+                        </Button>
+                    </div>
+                </div>
+            )
+        }
     }
 
 
@@ -139,7 +371,24 @@ const XpPopup = (props: IProps) => {
             <CssBaseline>
                 <Modal open={open} style={{display: 'flex', justifyContent: "center", alignItems: "center"}}>
                     <Box
-                        sx={{
+                        sx={showPro ? {
+                            width: "28vw",
+                            height: "62vh",
+                            minHeight: "420px",
+                            // justifyContent: "center",
+                            // marginLeft: "25vw",
+                            // marginTop: "5vh",
+                            outlineColor: "black",
+                            borderRadius: 7,
+                            boxShadow:
+                                "0px 12px 6px -6px rgba(0,0,0,0.6),0px 6px  0px rgba(0,0,0,0.6),0px 6px 18px 0px rgba(0,0,0,0.6)",
+                            // backgroundColor: theme.palette.background.default,
+                            backgroundImage: `url(${proBackground})`,
+                            backgroundSize: "cover",
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "center center"
+                            // ...themeHelpers.frostedGlass
+                        } : {
                             width: "40vw",
                             height: "45vh",
                             minHeight: "420px",
@@ -153,71 +402,7 @@ const XpPopup = (props: IProps) => {
                             backgroundColor: theme.palette.background.default,
                         }}
                     >
-                        {showLoot ? (
-                            <div>
-                                <LootPopup closePopup={closePopupLoot}
-                                           //@ts-ignore
-                                           reward={props.reward}/>
-                            </div>
-                        ) : (
-                            <div>
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        alignItems: "start",
-                                        justifyContent: "center",
-                                    }}
-                                >
-                                    <Fade cascade damping={1e-1} direction={"left"}>
-                                        <h1>{"You Earned " + xpTitle + " XP"}</h1>
-                                    </Fade>
-                                </div>
-                                <div style={{ height: "5vh" }} />
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        alignItems: "start",
-                                        justifyContent: "center",
-                                    }}
-                                >
-                                    <h3>{"Renown " +
-                                        renown}</h3>
-                                </div>
-                                <div
-                                    style={{
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        width: "100%",
-                                    }}
-                                >
-                                    <h4>{"Lvl " + currentLevel}</h4>
-                                    <div style={{ width: "20px" }} />
-                                    <Box sx={{ width: "75%"}}>
-                                        <StyledLinearProgress variant="determinate"/>
-                                    </Box>
-                                    <div style={{ width: "20px" }} />
-                                    <h4>{"Lvl " + nextLevel}</h4>
-                                </div>
-                                {showConfetti && (
-                                    <div style={{position: "absolute"}}>
-                                        <Lottie options={confettiOptions} isClickToPauseDisabled={true}
-                                                width={window.innerHeight / .8}
-                                                height={window.innerHeight / .8} style={{zIndex: 4}}/>
-                                    </div>
-                                )}
-                                <div style={{height: "5vh"}}/>
-                                <div style={{display: "flex", alignItems: "end", justifyContent: "center"}}>
-                                    <Button variant={"contained"}
-                                            sx={{ width: "25%", height: "60px", backgroundColor: "#29C18C", color: "white", borderRadius: "25px", boxShadow: '0 5px 0 #235d30',
-                                                '&:active':{transform: 'translateY(5px)', boxShadow: 'none'}, '&:hover':{backgroundColor: "#29C18C", cursor: 'pointer', '&:before': {transform: 'translateX(300px) skewX(-15deg)', opacity: 0.6, transition: '.7s'}}}} disableRipple={true} onClick={() => confirmButton()}
-                                            id={"button"} className={'button'} style={{zIndex: "600000"}}
-                                    >
-                                        Confirm
-                                    </Button>
-                                </div>
-                            </div>
-                        )}
+                        {renderXPPopup()}
                     </Box>
                 </Modal>
             </CssBaseline>
